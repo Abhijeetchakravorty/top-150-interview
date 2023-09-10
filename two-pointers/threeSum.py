1e5c296da966989e6b027ccd7b68b706fc00bc0d
Abhijeet Chakravorty <chakravorty.abhijeet91@gmail.com>
2023-09-10 23:22:54 +0530

    updated code

--- two-pointers/threeSum.py
+++ two-pointers/threeSum.py
@@ -1,39 +1,32 @@
 class Solution:
     #formula for adding all the nums
     def threeSum(self, nums):
-        
-    
+        res = []
+        nums.sort()
 
-
-
-
-
-    
-    
-    # def threeSum(self, nums):
-    #     res = []
-    #     nums.sort()
-
-    #     for k, v in enumerate(nums):
-    #         if k > 0 and v == nums[k-1]:
-    #             continue
-    #         l, r = k+1, len(nums) - 1
-    #         while l < r:
-    #             threeSum = v + nums[l] + nums[r]
-    #             if threeSum > 0:
-    #                 r -= 1
-    #             elif threeSum < 0:
-    #                 l += 1
-    #             else:
-    #                 res.append([v, nums[l], nums[r]])
-    #                 l += 1
-    #                 while (nums[l] == nums[l - 1]) and l < r:
-    #                     l+=1
-    #     return res
+        for i, a in enumerate(nums):
+            if i > 0 and a == nums[i-1]:
+                continue
+            l, r = i+1, len(nums) - 1
+            while l < r:
+                threeSum = a+nums[l]+nums[r]
+                if threeSum > 0:
+                    r -= 1
+                elif threeSum < 0:
+                    l += 1
+                else:
+                    res.append([a, nums[l], nums[r]])
+                    l += 1
+                    while nums[l]==nums[l-1] and l < r:
+                        l += 1
+        return res
 
 
 a = Solution()
 b = a.threeSum([-1,0,1,2,-1,-4])
+print(b)
+
+b = a.threeSum([-2, -2, 0, 0, 2, 2])
 print(b)
         
 
