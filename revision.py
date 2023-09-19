3489a313207554c631d7344d1940ac6fa5f0f52f
Abhijeet Chakravorty <chakravorty.abhijeet91@gmail.com>
2023-09-19 14:33:52 +0530

    update code for revision.py

--- revision.py
+++ revision.py
@@ -1,25 +1,41 @@
+from collections import defaultdict
 class Solution:
-    def topKFrequentElements(self, nums, k):
-        freq = [[] for i in range(len(nums)+1)]
-        res = []
-        hashmap = {}
-        for i in nums:
-            hashmap[i] = 1 + hashmap.get(i, 0)
-        
-        for c in hashmap:
-            freq[hashmap[c]].append(c)
+    def validSudoku(self, board):
+        cols = defaultdict(set)
+        rows = defaultdict(set)
+        squares = defaultdict(set)
 
-        for i in range(len(freq)-1, 0, -1):
-            j = 0
-            while j < len(freq[i]):
-                if len(res) == k:
-                    return res
-                res.append(freq[i][j])
-                j += 1
-        
-        return res
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+                if (
+                    board[r][c] in rows or
+                    board[r][c] in cols or
+                    board[r][c] in squares[(r//3, c//3)]
+                ):
+                    return False
+                rows[r].add(board[r][c])
+                cols[c].add(board[r][c])
+                squares[(r//3, c//3)].add(board[r][c])
+        print("\nRows", rows)
+
+        print("\nCols", rows)
+
+        print("\nSquares", rows)
+        return True
+
+
 
 a = Solution()
-b = a.topKFrequentElements([1, 1, 1, 2, 2, 3], 2)
-print("B: ", b)
+b = a.validSudoku([["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]])
 
+print("Ans: ", b)
