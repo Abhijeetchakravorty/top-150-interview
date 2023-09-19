db70bf5d89a439d1b5c08a679985d8a00a1786ac
Abhijeet Chakravorty <chakravorty.abhijeet91@gmail.com>
2023-09-19 16:41:28 +0530

    updated code for revision

--- revision.py
+++ revision.py
@@ -1,41 +1,24 @@
-from collections import defaultdict
 class Solution:
-    def validSudoku(self, board):
-        cols = defaultdict(set)
-        rows = defaultdict(set)
-        squares = defaultdict(set)
+    def encode(self, strs):
+        s = ""
+        for i in range(len(strs)):
+            s += str(len(strs[i]))+"#"+strs[i]
+        return s
 
-        for r in range(9):
-            for c in range(9):
-                if board[r][c] == ".":
-                    continue
-                if (
-                    board[r][c] in rows or
-                    board[r][c] in cols or
-                    board[r][c] in squares[(r//3, c//3)]
-                ):
-                    return False
-                rows[r].add(board[r][c])
-                cols[c].add(board[r][c])
-                squares[(r//3, c//3)].add(board[r][c])
-        print("\nRows", rows)
-
-        print("\nCols", rows)
-
-        print("\nSquares", rows)
-        return True
-
+    def decode(self, string):
+        res, i = [], 0
+        while i < len(string):
+            j = string.index("#", i)
+            length = int(string[i:j])
+            res.append(string[j+1:j+1+length])
+            i = j+1+length
+        return res
 
 
 a = Solution()
-b = a.validSudoku([["5","3",".",".","7",".",".",".","."]
-,["6",".",".","1","9","5",".",".","."]
-,[".","9","8",".",".",".",".","6","."]
-,["8",".",".",".","6",".",".",".","3"]
-,["4",".",".","8",".","3",".",".","1"]
-,["7",".",".",".","2",".",".",".","6"]
-,[".","6",".",".",".",".","2","8","."]
-,[".",".",".","4","1","9",".",".","5"]
-,[".",".",".",".","8",".",".","7","9"]])
 
-print("Ans: ", b)
+b = a.encode(["abcd", "abcd1", "abcd2"])
+print("B: ", b)
+
+c = a.decode(b)
+print("C: ", c)
