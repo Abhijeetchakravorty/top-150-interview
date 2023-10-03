568b0bd3cbe63ed46f03d543df68c835868fb194
Abhijeet Chakravorty <chakravorty.abhijeet91@gmail.com>
2023-10-04 04:41:00 +0530

    updated code

--- trees/lowest-common-ancestor-of-a-binary-search-tree.py
+++ trees/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -10,15 +10,26 @@ class Solution:
     # Space Complexity: O(1)
     def lowestCommonAncestor(self, root, p, q):
         while True:
-            # Check if the current root value is less than both p and q values.
+            # Check if the current root value is less 
+            # than both p and q values.
             if root.val < p.val and root.val < q.val:
-                root = root.right  # Move to the right subtree because both p and q are greater.
+                # Move to the right subtree because 
+                # both p and q are greater.
+                root = root.right  
             
-            # Check if the current root value is greater than both p and q values.
+            # Check if the current root 
+            # value is greater than both p and q values.
             elif root.val > p.val and root.val > q.val:
-                root = root.left  # Move to the left subtree because both p and q are smaller.
+                # Move to the left subtree 
+                # because both p and q are smaller.
+                root = root.left  
             
             else:
-                # If neither of the above conditions is met, it means the current root value
-                # is between p and q, or it is equal to either p or q, making it the lowest common ancestor.
-                return root  # Return the current root as the lowest common ancestor.
+                # If neither of the above 
+                # conditions is met, it means the current 
+                # root value is between p and q, or 
+                # it is equal to either p or q, 
+                # making it the lowest common ancestor.
+                # Return the current root as the lowest 
+                # common ancestor.
+                return root  
