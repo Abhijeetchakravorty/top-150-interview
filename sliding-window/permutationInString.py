0d6f1f69d6e725ac2a33aa736ef81c5347e5120f
Abhijeet Chakravorty <chakravorty.abhijeet91@gmail.com>
2023-10-09 19:46:54 +0530

    updated code for permutation in string

--- sliding-window/permutationInString.py
+++ sliding-window/permutationInString.py
@@ -14,12 +14,16 @@ class Solution:
         for i in range(len(s1)):
             s1_count[ord(s1[i]) - ord('a')] += 1
             s2_count[ord(s2[i]) - ord('a')] += 1
+        
+        print(s1_count, "\n", s2_count, "\n")
     
         for j in range(window_len, len(s2)):
             if s1_count == s2_count: return True
             
             s2_count[ord(s2[j-window_len]) - ord('a')] -= 1
             s2_count[ord(s2[j]) - ord('a')] += 1
+            print("S2_count After: ", s2_count)
+            print("\n")
         return s1_count == s2_count   
 
 
