2f10d52bbf2446b108d2f10f0502a5a40d8ee0b2
Abhijeet Chakravorty <chakravorty.abhijeet91@gmail.com>
2023-10-12 20:56:11 +0530

    updated code for serialize and deserialize codec

---
+++ trees/serialize-and-deserialize-binary-tree.py
@@ -0,0 +1,42 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Codec:
+    # Time Complexity : O(N)
+    # Space Complexity: O(N)
+    def serialize(self, root):
+        res = []
+
+        def dfs(node):
+            if not node:
+                res.append("N")
+                return
+            res.append(str(node.val))
+            dfs(node.left)
+            dfs(node.right)
+
+        dfs(root)
+        return ",".join(res)
+
+    # Time Complexity : O(N)
+    # Space Complexity: O(N)
+    def deserialize(self, data):
+        vals = data.split(",")
+        self.i = 0
+
+        def dfs():
+            if vals[self.i] == "N":
+                self.i += 1
+                return None
+            node = TreeNode(int(vals[self.i]))
+            self.i += 1
+            node.left = dfs()
+            node.right = dfs()
+            return node
+
+        return dfs()
