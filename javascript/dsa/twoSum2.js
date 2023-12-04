00d06f1700ad18fb08ca5cf43583e01919ea464f
Abhijeet Chakravorty <chakravorty.abhijeet91@gmail.com>
2023-12-04 23:59:55 +0530

    updated code for two sum

---
+++ javascript/dsa/twoSum2.js
@@ -0,0 +1,21 @@
+var twoSum = function (numbers, target) {
+    l = 0;
+    r = numbers.length - 1;
+    while (l <= r) {
+        let curSum = numbers[l] + numbers[r];
+        if (curSum > target) {
+            r -= 1;
+        }
+
+        if (curSum < target) {
+            l += 1;
+        }
+
+        if (curSum == target) {
+            return [l + 1, r + 1];
+        }
+    }
+    return [];
+};
+
+console.log(twoSum([2, 7, 11, 15], 9));
