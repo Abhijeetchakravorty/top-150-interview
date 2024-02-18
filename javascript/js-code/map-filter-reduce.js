34479e26db8670571421377d89863e4f28dc88c7
Abhijeet Chakravorty <chakravorty.abhijeet91@gmail.com>
2024-02-18 23:01:43 +0530

    udpated code for branch

--- javascript/js-code/map-filter-reduce.js
+++ javascript/js-code/map-filter-reduce.js
@@ -1,6 +1,7 @@
 const numbers = [1, 2, 3, 4, 5, 6];
-const squaredNumbers = numbers.map((num) => num * num);
-const filteredNumbers = numbers.filter((num) => num % 2 == 0);
-const sumNumbers = numbers.reduce((num, accumulator) => num + accumulator, 0);
+const squaredNumbers = numbers
+    .map((num) => num * num)
+    .filter((num) => num % 2 == 0)
+    .reduce((accumulator, num) => num + accumulator, 0);
 
-console.log(squaredNumbers, filteredNumbers, sumNumbers);
+console.log(squaredNumbers);
